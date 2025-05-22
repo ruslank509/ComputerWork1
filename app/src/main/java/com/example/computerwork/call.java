95a68b5c25503fb3218678a83eb9e67593ac763f
ruslank509 <ruslankarashulakov@gmail.com>
2025-05-22 09:54:17 +0400

    Были убраны неиспользованные импорты

--- app/src/main/java/com/example/computerwork/call.java
+++ app/src/main/java/com/example/computerwork/call.java
@@ -2,7 +2,7 @@ package com.example.computerwork;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +14,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import okhttp3.MediaType;
 
 public class call extends AppCompatActivity {
 
